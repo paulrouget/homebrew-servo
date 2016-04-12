168c57e4d619673d219221b772f1d4b0d6ff905f
Paul Rouget <me@paulrouget.com>
2016-04-12 10:40:45 +0800

    update

--- Formula/servo-binary.rb
+++ Formula/servo-binary.rb
@@ -1,9 +1,9 @@
 class ServoBinary < Formula
   desc "Servo, the Parallel Browser Engine Project (binary version)"
   homepage "http://servo.org"
-  url "http://people.mozilla.org/~prouget/graphene/2016-04-07/servo-release-hotfix-mac-2016-04-07.zip"
-  version "2016.04.07"
-  sha256 "cc261001387b7ddf24fed34d98881a5c42ffeed8a2e1f6ac7ad05b9d66b6af23"
+  url "http://people.mozilla.org/~prouget/graphene/2016-04-11/servo-release-hotfix-mac-2016-04-11.zip"
+  version "2016.04.11"
+  sha256 "18c27b6e5d5dd772636780a6560f1e977407a890f7f5a4f4b9636a86c8220539"
 
   bottle :unneeded
 
